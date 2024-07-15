55348014612e31a086ea599210a13cc532b79e17
Smalligbig <smalligbig@gmail.com>
2024-07-15 09:33:20 +0800

    Update SerinBypassMain.lua

--- SerinBypassC/SerinBypassMain.lua
+++ SerinBypassC/SerinBypassMain.lua
@@ -1,12 +1,11 @@
 local options = ({ ... })[1] or { Method = 1 }
 local Method = options.Method
-local MethodsList = { [1] = { ["A"] = "А", ["B"] = "Β", ["C"] = "С", ["D"] = "Ɗ", ["E"] = "Е", ["F"] = "Ƒ", ["G"] = "Ğ"}, [2] = {["A"] = "А"}}
-
+local MethodsList = { [1] = { ["A"] = "А", ["B"] = "Β", ["C"] = "С", ["D"] = "Ɗ", ["E"] = "Е", ["F"] = "Ƒ", ["G"] = "Ğ"}, 
+  [2] = {["A"] = "А"}}
 function RepLet(text, MethodsList)
 local MethodTable = MethodsList[Method] or {}
 local result = ""
-result = text:gsub(".", function(char) return MethodTable[char] or char end) return result
+result = text:gsub(".", function(char) return MethodTable[char] or char end) return result 
 end
-
 local test = RepLet("GAG", MethodsList)
 print(test)
