989e4692ff3aa6d4ceabf6d175c57f5f25ebf9ac
Smalligbig <smalligbig@gmail.com>
2024-07-26 21:26:19 +0800

    Update SerinBypassMain.lua

--- SerinBypassC/SerinBypassMain.lua
+++ SerinBypassC/SerinBypassMain.lua
@@ -1,7 +1,7 @@
 local options = ({ ... })[1] or { Method = 1 }
 local Method = options.Method
 local filler = "︠︠︠ ॔॔॔ ̈́͊̈́͊̈́͊̈́͊̈́͊̓҄ ॓॓॓॓॓॓॓॓⬞  ॓ ိ   ိ  " 
-local filler2 = ""
+local filler2 = "FAG "
 local MethodsList = {
   [1] = { [" "] = "", ["A"] = "А", ["B"] = "В", ["C"] = "С", ["E"] = "Е", ["G"] = "Ġ", ["H"] = "Н", ["I"] = "І", ["J"] = "Ј", ["K"] = "К",  ["N"] = "Ν", ["O"] = "О", ["P"] = "Р",  ["R"] = "Ṛ̘̙̝̞", ["S"] = "Ѕ", ["T"] = "Т", ["U"] = "∪",  ["X"] = "Х", ["Y"] = "Ү", ["a"] = "а", ["b"] = "ḅ", ["c"] = "с", ["e"] = "е", ["g"] = "ɡ", ["h"] = "һ", ["i"] = "і", ["j"] = "ј", ["k"] = "к",  ["n"] = "ṅ", ["o"] = "о", ["p"] = "р", ["r"] = "ṛ̘̙̝̞", ["s"] = "ѕ", ["t"] = "ʈ", ["u"] = "υ",  ["x"] = "х", ["y"] = "у"}, 
   [2] = { [" "] = "", ["A"] = "А"}}
@@ -11,11 +11,10 @@ local result = ""
 result = text:gsub(".", function(char) return MethodTable[char] or char end) 
   if Method == 1 then 
   return filler .. result
+  elseif Method == 2 then
+    return filler2 .. result
   end
-  if Method == 2 then
-  return filler2 .. result
-  end
-  return result
+
 end
 local test = RepLet("HI", MethodsList)
 print(test)
