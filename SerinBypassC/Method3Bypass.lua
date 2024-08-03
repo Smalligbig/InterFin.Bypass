2cb5b506951a1da1760f10c62c3271f1670d631f
Smalligbig <smalligbig@gmail.com>
2024-08-03 13:14:56 +0800

    Update Method3Bypass.lua

--- SerinBypassC/Method3Bypass.lua
+++ SerinBypassC/Method3Bypass.lua
@@ -8,4 +8,69 @@ Serin Bypass Method 3
 ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░             ░▒▓█▓▒░ 
 ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░       ░▒▓███████▓▒░     
 ]] 
+--[[ Bypassed Phrases
+homophobic >
+Who let this gay dude out of the torture cell
+Get this gay dude in the torture cell
+Burn the lgbtq flag
+The stripes on lgbtq are fucking straight
+There's only 2 genders you gay bitch
+Arrest every gay people
+Kill gay peoples on a cotton field
+Dont turn gay bitches
+Yall gays mad because i call all lgbtq gay
+Dont be transgender they are shit
+Hang all lgbtq people
+Theres only 2 genders you fucking gays
 
+sexual >
+Lemme steal yo cock
+Lets have sex to get you pregnant
+Holy fuck im gonna cumm!
+Im horny for you😩
+Im gonna fuckin bang you
+Your pussy is juicy as fuck i might cum😩
+CMON BITCH SUCK MY COCK
+Lemme smack that cock
+*Looks at your cock* Wait lemme suck on it
+I HAD SEX WITH NO CONDOM IM A PRO😎
+Im gonna make you pregnant
+I JUST SHOT UP A SCHOOL AND HAD SEX WITH EVERY DEAD BODY
+
+racist >
+Blacks niggerusly doesn't matter
+Get back to the cotton field
+HEY NIGGA DONT TRY TO ESCAPE FROM THE COTTON FIELD
+Hit niggers with a bat
+Hang niggers front of his family
+Cage slaves that tried to escape
+Siege heil
+Asians must be executed by firing rice
+I SHALL KILL ALL NIGGERS ON THIS WORLD
+This server is full of niggers
+Ewwww is that a nigga shoot that nigga
+Dismantle niggers to not escape from cotton field
+
+Insults >
+Kys nigga nigga
+Go drink your own piss nigga
+THIS BTICHES DICK IS SMALL LMAO
+Your virgin arent you? 
+Your black aren't you? 
+Imagine hating on hitler LMAO
+Go kys faggot
+Your a fucking faggot
+Nigga what?
+Your just mad because my dick is longer
+Stfu dumbass
+Munch on corn dumbass
+
+Compliments >
+Holy fuck your dick is so long
+Can i suck your penis? 
+Wow your penis isnt small
+Can i drink your penis piss? 
+*Drinks piss from penis*
+Your dick is so long i wanna suck on it
+Your dick long i wanna suck it
+]] 
