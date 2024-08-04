d61bc79a29c38ad59c1bd724a0fb23371d8aa5de
Smalligbig <smalligbig@gmail.com>
2024-08-04 11:50:44 +0800

    Update SerinBypassMain.lua

--- SerinBypassC/SerinBypassMain.lua
+++ SerinBypassC/SerinBypassMain.lua
@@ -43,11 +43,14 @@ local MethodsList = {
   [1] = { ["A"] = "Α", ["B"] = "В", ["C"] = "С", ["E"] = "Е", ["G"] = "Ġ", ["H"] = "Н", ["I"] = "І", ["J"] = "Ј", ["K"] = "Κ",  ["N"] = "Ν", ["O"] = "О", ["P"] = "Р", ["S"] = "Ѕ", ["T"] = "Т", ["U"] = "∪", ["V"] = "Ѵ", ["X"] = "Х", ["Y"] = "Ү", ["a"] = "а", ["b"] = "ხ", ["c"] = "с", ["e"] = "е", ["g"] = "ɡ", ["h"] = "һ", ["i"] = "і", ["j"] = "ј", ["k"] = "ҟ",  ["n"] = "ท", ["o"] = "о", ["p"] = "р", ["s"] = "ร", ["t"] = "ʈ", ["u"] = "น", ["v"] = "ѵ", ["x"] = "х", ["y"] = "у"}, 
   [2] = { [" "] = "", ["A"] = "А"}}
 function RepLet(text, MethodsList)
-local MethodTable = MethodsList[Method] or {}
-local result = ""
-result = text:gsub(".", function(char) return MethodTable[char] or char end) 
-  return filler .. result
-end
+    local MethodTable = MethodsList[Method] or {}
+    local result = ""
+    for i = 1, #text do
+        local char = text:sub(i, i)
+        result = result .. (MethodTable[char] or char)
+    end
+    return filler .. result
+ end
 local test = RepLet("Testing", MethodsList)
 print(test)
 else game.Loaded:Wait() end
