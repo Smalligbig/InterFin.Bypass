680e2c5415085bed1dadee1b57fa2fc7e80a8892
Smalligbig <smalligbig@gmail.com>
2024-08-06 21:10:08 +0800

    Update SerinBypass.lua

--- SerinBypass.lua
+++ SerinBypass.lua
@@ -1,2 +1,2 @@
 -- Serin bypass by Dansk/Smalligbig --
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Smalligbig/SerinBypassRblx/main/SerinBypassC/SerinBypassMain.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Smalligbig/SerinBypassRblx/main/SerinBypassC/SerinBypassMain.lua"))()
