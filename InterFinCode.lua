51b10532a9792b99f61b1ebc4641ed02f8d141eb
Smalligbig <smalligbig@gmail.com>
2024-09-19 06:34:37 +0800

    Update InterFinCode.lua

--- InterFinCode.lua
+++ InterFinCode.lua
@@ -14,4 +14,4 @@
 (_____)----------------------------------------------------------(_____)
 
 ]]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Smalligbig/InterFin.Bypass/refs/heads/main/Codes/InterFin.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Smalligbig/InterFin.Bypass/refs/heads/main/Codes/SOURCE.lua"))()
